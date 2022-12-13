91b2c6ea8d4ea5b9f1cc3084c2e0bbe2d6c69262
HalHarris1501 <56324460+HalHarris1501@users.noreply.github.com>
2022-12-13 23:20:39 +0000

    Fixed the camera follow object script and created the Input Controller script
    Will work on Input Controller Script tomorrow morning

--- D&D Roguelike/Assets/Scripts/FollowObject.cs
+++ D&D Roguelike/Assets/Scripts/FollowObject.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class FollowObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject objectToFollow;
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y, -1f);
     }
 }
