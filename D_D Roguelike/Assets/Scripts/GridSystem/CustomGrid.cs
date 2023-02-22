bd0db9cef1c8f5aec459e78d42b8cbd90f93a1aa
HalHarris1501 <56324460+HalHarris1501@users.noreply.github.com>
2023-02-22 20:20:49 +0000

    Added ability to use multiple object types with the grid by making it accept generics

---
+++ D_D Roguelike/Assets/Scripts/GridSystem/CustomGrid.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using System;
+
+public class CustomGrid <TGridObject>
+{
+
+    public event EventHandler<OnGridValueChangedEventArgs> OnGridValueChanged;
+    public class OnGridValueChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private TGridObject[,] gridArray;
+
+    public CustomGrid(int width, int height, float cellSize, Vector3 originPosition, Func<CustomGrid<TGridObject>, int, int, TGridObject> createGridObject)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new TGridObject[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                gridArray[x, y] = createGridObject(this, x, y);
+            }
+        }
+
+        bool showDebug = true;
+        if (showDebug)
+        {
+            TextMesh[,] debugTextArray = new TextMesh[width, height];
+
+            for (int x = 0; x < gridArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < gridArray.GetLength(1); y++)
+                {
+                    //make text in middle of cell
+                    debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y]?.ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, Mathf.FloorToInt(cellSize/0.1f), Color.green, TextAnchor.MiddleCenter);
+                    //draw cell walls
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.green, 100f);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.green, 100f);
+                }
+            }
+            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.green, 100f);
+            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.green, 100f);
+
+            OnGridValueChanged += (object sender, OnGridValueChangedEventArgs eventArgs) =>
+            {
+                debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y]?.ToString();
+            };
+        }
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetGridObject(int x, int y, TGridObject value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            OnGridValueChanged?.Invoke(this, new OnGridValueChangedEventArgs { x = x, y = y });
+        }
+    }
+
+    public void TriggerGridObjectChanged(int x, int y)
+    {
+        OnGridValueChanged?.Invoke(this, new OnGridValueChangedEventArgs { x = x, y = y });
+    }
+
+    public void SetGridObject(Vector3 worldPosition, TGridObject value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetGridObject(x, y, value); 
+    }
+
+    public TGridObject GetGridObject(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return default(TGridObject);
+        }
+    }
+
+    public TGridObject GetGridObject(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetGridObject(x, y);
+    }
+}
--- D_D Roguelike/Assets/Scripts/GridSystem/Testing.cs
+++ D_D Roguelike/Assets/Scripts/GridSystem/Testing.cs
@@ -1,13 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Testing : MonoBehaviour
 {
+    private CustomGrid<HeatmapGridObject> grid;
+
     // Start is called before the first frame update
     private void Start()
     {
-        Grid grid = new Grid(20, 10);
+        grid = new CustomGrid<HeatmapGridObject>(150, 100, 1f, new Vector3(-10, -10), (CustomGrid<HeatmapGridObject> g, int x, int y) => new HeatmapGridObject(g, x, y));
     }
 
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 position = UtilsClass.GetMouseWorldPosition();
+            HeatmapGridObject heatmapGridObject = grid.GetGridObject(position);
+            if(heatmapGridObject != null)
+            {
+                heatmapGridObject.AddValue(5);
+            }
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetGridObject(UtilsClass.GetMouseWorldPosition())); 
+        }
+    }
+}
+
+public class HeatmapGridObject
+{
+    private const int MIN = 0;
+    private const int MAX = 100;
+
+    private CustomGrid<HeatmapGridObject> grid;
+    private int x;
+    private int y;
+    private int value;
+
+    public HeatmapGridObject(CustomGrid<HeatmapGridObject> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void AddValue(int addValue)
+    {
+        value += addValue;
+        value = Mathf.Clamp(value, MIN, MAX);
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public float GetNormalizedValue()
+    {
+        return (float)value / MAX;
+    }
+
+    public override string ToString()
+    {
+        return value.ToString();
+    }
 }
